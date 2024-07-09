f7162eec286b78cf69d37da33be4341b6d682019
Your Name <noriaki_igarashi@nuco.co.jp>
2024-07-09 14:05:24 +0900

    My Django Girls app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 def post_list(request):
     return render(request, 'blog/post_list.html',{})
+    user = User.objects.get(username='your_username')
+    post = Post(author=user, title='Sample title', text='Test content', created_date=timezone.now())
+    post.save()
 # Create your views here.
