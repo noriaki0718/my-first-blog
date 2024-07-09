05ac1adbcff8d3c051923a250716fb29cc821aa2
Your Name <noriaki_igarashi@nuco.co.jp>
2024-07-09 14:59:41 +0900

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from .models import Post
 from django.utils import timezone
-from django.contrib.auth.models import User
+from .models import Post
 
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{})
-    user = User.objects.get(username='your_username')
-    post = Post(author=user, title='Sample title', text='Test content', created_date=timezone.now())
-    post.save()
 # Create your views here.
